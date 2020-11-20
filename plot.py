8a518cdec1b0e7560b140fa9a16fdbb8470b8815
Christiaan Lombard <base1.christiaan@gmail.com>
2020-11-20 22:17:35 +0200

    notebook done, model working

--- plot.py
+++ plot.py
@@ -32,9 +32,25 @@ def plot_image(image, ax=None):
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
+    ax.axis('off')
     ax.tick_params(axis='both', length=0)
     ax.set_xticklabels('')
     ax.set_yticklabels('')
     ax.imshow(image)
 
     return ax
+
+def plot_image_prediction(image, ps_values, ps_classes, ps_labels):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,3), ncols=2)
+    plot_image(image, ax1)
+    ax2.barh(np.arange(len(ps_values)), ps_values)
+    # ax2.set_aspect(0.1)
+    # ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels([ps_labels], size='small')
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1)
+
+    plt.tight_layout()
